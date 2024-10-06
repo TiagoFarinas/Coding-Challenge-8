574b6c292e48e6c55b49165ac3bbb3927c2f8628
Tiago Farinas <tiagofarinas@gmail.com>
2024-10-06 17:40:30 -0400

    Create Department class to manage employees and calculate salary(task 2) +Handle bonuses in the department salary calculation(task4)

--- employee_management.js
+++ employee_management.js
@@ -8,3 +8,26 @@ class Employee {
         this.type = 'Employee'}
     getDetails() {
         console.log(`Name: ${this.name}, Position: ${this.position}, Salary: $${this.salary}`)}}
+
+        //Task 2-Create a Department Class
+
+        class Department {
+            constructor(name) {
+                this.name = name;
+                this.employees = []};
+            addEmployee(employee) {
+                if (employee && (employee.type === 'Employee' || employee.type === 'Manager')) {
+                    this.employees.push(employee)}};
+            getDepartmentSalary() {
+                let totalSalary = 0;
+                for (const employee of this.employees) {
+                    totalSalary += employee.salary};
+                return totalSalary};
+            calculateTotalSalaryWithBonus() {// Task 4: Handle Bonuses in the Department Salary Calculation
+                let totalSalaryWithBonus = 0;
+                for (const employee of this.employees) {
+                    let bonus = 0; // Initialize bonus
+                    if (employee.type === 'Manager') {
+                        bonus = employee.bonus}
+                    totalSalaryWithBonus += employee.salary + bonus};
+                return totalSalaryWithBonus}};
